6b9c9f7cc5b52ad973ca297a14d2ab45ebb74a1a
Muhammad Abdiel Firjatullah <abdielfirdie@gmail.com>
2023-03-04 14:31:26 +0700

    setup config

--- routes/web.php
+++ routes/web.php
@@ -16,11 +16,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-Route::group(['middleware' => ['auth:sanctum', 'verified']], function() {
+Route::group(['middleware' => ['auth:sanctum', 'verified']], function () {
     Route::get('/dashboard', function () {
         return view('dashboard');
     })->name('dashboard');
-    Route::group(['prefix' => 'components', 'as' => 'components.'], function() {
+    Route::group(['prefix' => 'components', 'as' => 'components.'], function () {
         Route::get('/alert', function () {
             return view('admin.component.alert');
         })->name('alert');
